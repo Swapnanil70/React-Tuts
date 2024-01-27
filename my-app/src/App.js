5d5be7c5f9dea1ef877512c2d94a5e5c638ac50a
swapnanil-14963 <swapnanil-14963 @csez.zohocorpin.com>
2024-01-27 16:23:16 +0530

    Added the JSX understanding changes.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+let name = 'Neel';
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {user}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Neel
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      {getGreeting(name)} <br /> {getGreeting()}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus nam fuga illo veritatis? Aperiam fugit suscipit asperiores sequi consequuntur eius officiis est voluptates ex, 
+        quia necessitatibus ipsam. Facilis asperiores sit nisi labore sint error!
+      </p>
     </div>
+    </>
   );
 }
 
